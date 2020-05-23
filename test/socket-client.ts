0a20d5178140cd8dd84bfd6247d385f480920629
rajibchy <mysuccessstairs@hotmail.com>
2020-05-24 04:21:40 +0600

    Improve test

--- test/socket-client.ts
+++ test/socket-client.ts
@@ -1,3 +1,4 @@
+import expect from 'expect';
 import { wsClient } from '../index';
 const clientInfo = wsClient();
 clientInfo.on( "beforeInitiateConnection", ( session, socket ) => {
@@ -8,8 +9,18 @@ clientInfo.on( "beforeInitiateConnection", ( session, socket ) => {
 } );
 clientInfo.on( "getClient", ( me, session, wsServer, server ) => {
     const _client = {
-        'test-msg': (data: any) => {
-            return me.sendMsg( "on-test-msg", data );
+        'test-msg': ( data: any ) => {
+            expect( me.getSocket() ).toBeDefined();
+            expect( wsServer.isActiveSocket( me.token ) ).toEqual( true );
+            expect( wsServer.getOwners( me.group ).length ).toEqual( 0 );
+            expect( wsServer.getOwners().length ).toEqual( 0 );
+            expect( wsServer.findByHash( me.hash || "no_hash" ).length ).toEqual( 0 );
+            expect( wsServer.findByLogin( me.loginId || "un_authorized" ).length ).toEqual( 0 );
+            expect( wsServer.toList( wsServer.getClientByExceptHash( me.hash || "no_hash" ) ).length ).toBeGreaterThan( 0 );
+            expect( wsServer.getClientByExceptHash( me.hash || "no_hash", me.group || "no_group" ).length ).toEqual( 0 );
+            expect( wsServer.getClientByExceptLogin( me.loginId || "un_authorized" ).length ).toBeGreaterThan( 0 );
+            expect( wsServer.getClientByExceptLogin( me.loginId || "un_authorized", me.group || "no_group" ).length ).toEqual( 0 );
+            return wsServer.sendMsg( me.token, "on-test-msg", data );
         }
     };
     return !me ? {
@@ -18,24 +29,24 @@ clientInfo.on( "getClient", ( me, session, wsServer, server ) => {
     } : _client;
 } );
 clientInfo.on( "connected", ( me, wsServer ) => {
-    // const method = me.isReconnectd ? "on-re-connected-user" : "on-connect-user";
-    // wsServer.getClientByExceptToken( me.token ).forEach( conn => {
-    //    conn.sendMsg( method, {
-    //        token: me.token, hash: me.hash, loginId: me.loginId
-    //    } );
-    // } );
-    // Here connect any user
-    // me.sendMsg( me.isReconnectd ? "on-re-connected" : "on-connected", {
-    //    token: me.token, hash: me.hash, loginId: me.loginId
-    // } );
+     const method = me.isReconnectd ? "on-re-connected-user" : "on-connect-user";
+     wsServer.getClientByExceptToken( me.token ).forEach( conn => {
+        conn.sendMsg( method, {
+            token: me.token, hash: me.hash, loginId: me.loginId
+        } );
+     } );
+     // Here connect any user
+     me.sendMsg( me.isReconnectd ? "on-re-connected" : "on-connected", {
+        token: me.token, hash: me.hash, loginId: me.loginId
+     } );
 } );
 clientInfo.on( "disConnected", ( me, wsServer ) => {
-    // Here disconnect any user
-    // wsServer.getClientByExceptToken( me.token ).forEach( conn => {
-    //    conn.sendMsg( "on-disconnected-user", {
-    //        token: me.token, hash: me.hash, loginId: me.loginId
-    //    } );
-    // } );
+     // Here disconnect any user
+     wsServer.getClientByExceptToken( me.token ).forEach( conn => {
+        conn.sendMsg( "on-disconnected-user", {
+            token: me.token, hash: me.hash, loginId: me.loginId
+        } );
+     } );
 } );
 export function SocketClient() {
     return clientInfo;
