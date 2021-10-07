19e200309b8c8dd21958dcfc1986af1a2ba7d104
Rajib Chowdhury <mysuccessstairs@hotmail.com>
2021-10-07 18:48:19 +0600

    Update app security..

--- dist/lib/sow-logger.d.ts
+++ dist/lib/sow-logger.d.ts
@@ -1,5 +1,6 @@
 import { IDispose } from './sow-static';
 export interface ILogger extends IDispose {
+    isProduction: boolean;
     newLine(): any;
     write(msg: string, color?: string): ILogger;
     log(msg: string, color?: string): ILogger;
@@ -9,6 +10,7 @@ export interface ILogger extends IDispose {
     reset(): ILogger;
     writeToStream(str: string): void;
     flush(): boolean;
+    writeBuffer(msg: string): void;
 }
 export declare class LogTime {
     static dfo(t: number): string;
@@ -16,32 +18,49 @@ export declare class LogTime {
     static getLocalDateTime(offset: any): Date;
     static getTime(tz: string): string;
 }
+declare type IColor = string;
 export declare class ConsoleColor {
-    static Cyan(str: string): string;
-    static Yellow(str: string): string;
-    static Reset: string;
-    static Bright: string;
-    static Dim: string;
-    static Underscore: string;
-    static Blink: string;
-    static Reverse: string;
-    static Hidden: string;
-    static FgBlack: string;
-    static FgRed: string;
-    static FgGreen: string;
-    static FgYellow: string;
-    static FgBlue: string;
-    static FgMagenta: string;
-    static FgCyan: string;
-    static FgWhite: string;
-    static BgBlack: string;
-    static BgRed: string;
-    static BgGreen: string;
-    static BgYellow: string;
-    static BgBlue: string;
-    static BgMagenta: string;
-    static BgCyan: string;
-    static BgWhite: string;
+    static Cyan(str: string): IColor;
+    static Yellow(str: string): IColor;
+    static Reset: IColor;
+    static Bright: IColor;
+    static Dim: IColor;
+    static Underscore: IColor;
+    static Blink: IColor;
+    static Reverse: IColor;
+    static Hidden: IColor;
+    static FgBlack: IColor;
+    static FgRed: IColor;
+    static FgGreen: IColor;
+    static FgYellow: IColor;
+    static FgBlue: IColor;
+    static FgMagenta: IColor;
+    static FgCyan: IColor;
+    static FgWhite: IColor;
+    static BgBlack: IColor;
+    static BgRed: IColor;
+    static BgGreen: IColor;
+    static BgYellow: IColor;
+    static BgBlue: IColor;
+    static BgMagenta: IColor;
+    static BgCyan: IColor;
+    static BgWhite: IColor;
+}
+export declare class ShadowLogger implements ILogger {
+    private _isProduction;
+    get isProduction(): boolean;
+    constructor();
+    writeBuffer(msg: string): void;
+    newLine(): void;
+    write(msg: string, color?: IColor): ILogger;
+    log(msg: string, color?: IColor): ILogger;
+    info(msg: string): ILogger;
+    success(msg: string): ILogger;
+    error(msg: string): ILogger;
+    reset(): ILogger;
+    writeToStream(str: string): void;
+    flush(): boolean;
+    dispose(): void;
 }
 export declare class Logger implements ILogger {
     private _userInteractive;
@@ -52,17 +71,21 @@ export declare class Logger implements ILogger {
     private _blockSize;
     private _maxBlockSize;
     private _fd;
+    private _isProduction;
+    get isProduction(): boolean;
     constructor(dir?: string, name?: string, tz?: string, userInteractive?: boolean, isDebug?: boolean, maxBlockSize?: number);
     flush(): boolean;
     writeToStream(str: string): void;
     newLine(): void;
     private _write;
+    writeBuffer(buffer: string): void;
     private _log;
-    write(msg: any, color?: string): ILogger;
-    log(msg: any, color?: string): ILogger;
+    write(msg: any, color?: IColor): ILogger;
+    log(msg: any, color?: IColor): ILogger;
     info(msg: any): ILogger;
     success(msg: any): ILogger;
     error(msg: any): ILogger;
     reset(): ILogger;
     dispose(): void;
 }
+export {};
